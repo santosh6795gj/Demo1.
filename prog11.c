ec16379f7d8742038c5c26874e866988fe8bf14f
santosh6795gj <93151312+santosh6795gj@users.noreply.github.com>
2022-01-26 19:16:36 +0530

    Demo.c
    Demo.c

---
+++ prog11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void Display();  // function Declration 
+
+int main()
+{
+
+    Display();   // Funtion  call 
+
+    return 0;
+}
+
+void Display()   // Function Defintion 
+{
+    int i = 0;
+    for(i = 1; i<=5; i++)
+    {
+        printf(" Marvellous",i);
+
+    }
+}

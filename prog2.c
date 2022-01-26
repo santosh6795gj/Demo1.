a13346e26202fd07974571b77f4aae45f02e65e1
santosh6795gj <93151312+santosh6795gj@users.noreply.github.com>
2022-01-26 19:00:09 +0530

    Demo.c
    Demo.c

---
+++ prog2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int x,y,z;
+
+    printf("Enter First Number\n");
+    scanf("%d",&x);
+
+    printf("Enter Scond Number\n");
+    scanf("%d",&y);
+
+    z = x + y;
+
+    printf(" Addition is...%d\n",z);
+
+    return 0;
+
+}

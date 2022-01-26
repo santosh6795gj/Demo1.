c93e6461c9a8fe3e0398ed5b06adf7d6bd4bbd17
santosh6795gj <93151312+santosh6795gj@users.noreply.github.com>
2022-01-26 19:04:01 +0530

    Demo.c
    Demo.c

---
+++ prog6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+   int i = 0;
+
+   for (i = 1; i<=5; i++)
+   {
+       printf("%d\n",i);
+ 
+   }
+       return 0;
+}
+
+// Strating point  : 1
+// Ending point : 5
+// Displacment  : 1

a898262f5cb84d9c444f87e6e137cd80abaa3bec
santosh6795gj <93151312+santosh6795gj@users.noreply.github.com>
2022-01-26 19:05:18 +0530

    Demo.c
    Demo.c

---
+++ prog8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+void Display();
+
+int main()
+{
+
+    Display();
+
+    return 0;
+}
+
+void Display()
+{
+    int i = 0;
+    for(i = 1; i<=5; i++)
+    {
+        printf("%d",i);
+
+    }
+}
